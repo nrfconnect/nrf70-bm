d54812adde9821a7351e48ca985b75a054304514
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2024-09-05 02:52:46 +0530

    bm_lib: Fix DTS parameters
    Use the Zephyr DTS infrastructure to avoid re-inventing as DTS support
    is common in most OSes.
    
    This fixes the zero TX power issue on boot using radio test.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- nrf70_bm_lib/source/nrf70_bm_core.c
+++ nrf70_bm_lib/source/nrf70_bm_core.c
@@ -7,6 +7,10 @@
 /** @file
  * @brief nRF70 Bare Metal initialization.
  */
+
+/* Should be provided by the Platform/OS */
+#include <devicetree.h>
+
 #include "nrf70_bm_core.h"
 #include "nrf70_bm_lib.h"
 
@@ -225,6 +229,102 @@ static void nrf_wifi_event_proc_disp_scan_res_zep(void *vif_ctx,
 }
 #endif /* CONFIG_NRF70_RADIO_TEST */
 
+static void configure_tx_pwr_settings(struct nrf_wifi_tx_pwr_ctrl_params *tx_pwr_ctrl_params,
+				struct nrf_wifi_tx_pwr_ceil_params *tx_pwr_ceil_params)
+{
+	tx_pwr_ctrl_params->ant_gain_2g = CONFIG_NRF700X_ANT_GAIN_2G;
+	tx_pwr_ctrl_params->ant_gain_5g_band1 = CONFIG_NRF700X_ANT_GAIN_5G_BAND1;
+	tx_pwr_ctrl_params->ant_gain_5g_band2 = CONFIG_NRF700X_ANT_GAIN_5G_BAND2;
+	tx_pwr_ctrl_params->ant_gain_5g_band3 = CONFIG_NRF700X_ANT_GAIN_5G_BAND3;
+	tx_pwr_ctrl_params->band_edge_2g_lo_dss = CONFIG_NRF700X_BAND_2G_LOWER_EDGE_BACKOFF_DSSS;
+	tx_pwr_ctrl_params->band_edge_2g_lo_ht = CONFIG_NRF700X_BAND_2G_LOWER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_2g_lo_he = CONFIG_NRF700X_BAND_2G_LOWER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_2g_hi_dsss = CONFIG_NRF700X_BAND_2G_UPPER_EDGE_BACKOFF_DSSS;
+	tx_pwr_ctrl_params->band_edge_2g_hi_ht = CONFIG_NRF700X_BAND_2G_UPPER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_2g_hi_he = CONFIG_NRF700X_BAND_2G_UPPER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_1_lo_ht =
+		CONFIG_NRF700X_BAND_UNII_1_LOWER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_1_lo_he =
+		CONFIG_NRF700X_BAND_UNII_1_LOWER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_1_hi_ht =
+		CONFIG_NRF700X_BAND_UNII_1_UPPER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_1_hi_he =
+		CONFIG_NRF700X_BAND_UNII_1_UPPER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_2a_lo_ht =
+		CONFIG_NRF700X_BAND_UNII_2A_LOWER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_2a_lo_he =
+		CONFIG_NRF700X_BAND_UNII_2A_LOWER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_2a_hi_ht =
+		CONFIG_NRF700X_BAND_UNII_2A_UPPER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_2a_hi_he =
+		CONFIG_NRF700X_BAND_UNII_2A_UPPER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_2c_lo_ht =
+		CONFIG_NRF700X_BAND_UNII_2C_LOWER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_2c_lo_he =
+		CONFIG_NRF700X_BAND_UNII_2C_LOWER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_2c_hi_ht =
+		CONFIG_NRF700X_BAND_UNII_2C_UPPER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_2c_hi_he =
+		CONFIG_NRF700X_BAND_UNII_2C_UPPER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_3_lo_ht =
+		CONFIG_NRF700X_BAND_UNII_3_LOWER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_3_lo_he =
+		CONFIG_NRF700X_BAND_UNII_3_LOWER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_3_hi_ht =
+		CONFIG_NRF700X_BAND_UNII_3_UPPER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_3_hi_he =
+		CONFIG_NRF700X_BAND_UNII_3_UPPER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_4_lo_ht =
+		CONFIG_NRF700X_BAND_UNII_4_LOWER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_4_lo_he =
+		CONFIG_NRF700X_BAND_UNII_4_LOWER_EDGE_BACKOFF_HE;
+	tx_pwr_ctrl_params->band_edge_5g_unii_4_hi_ht =
+		CONFIG_NRF700X_BAND_UNII_4_UPPER_EDGE_BACKOFF_HT;
+	tx_pwr_ctrl_params->band_edge_5g_unii_4_hi_he =
+		CONFIG_NRF700X_BAND_UNII_4_UPPER_EDGE_BACKOFF_HE;
+
+
+	tx_pwr_ceil_params->max_pwr_2g_dsss =
+			DT_PROP(DT_NODELABEL(nrf70_tx_power_ceiling), max_pwr_2g_dsss);
+
+	tx_pwr_ceil_params->max_pwr_2g_mcs7 =
+		DT_PROP(DT_NODELABEL(nrf70_tx_power_ceiling), max_pwr_2g_mcs7);
+
+	tx_pwr_ceil_params->max_pwr_2g_mcs0 =
+		DT_PROP(DT_NODELABEL(nrf70_tx_power_ceiling), max_pwr_2g_mcs0);
+
+#ifndef CONFIG_NRF70_2_4G_ONLY
+	tx_pwr_ceil_params->max_pwr_5g_low_mcs7 =
+		DT_PROP(DT_NODELABEL(nrf70_tx_power_ceiling), max_pwr_5g_low_mcs7);
+
+	tx_pwr_ceil_params->max_pwr_5g_mid_mcs7 =
+		DT_PROP(DT_NODELABEL(nrf70_tx_power_ceiling), max_pwr_5g_mid_mcs7);
+
+	tx_pwr_ceil_params->max_pwr_5g_high_mcs7 =
+		DT_PROP(DT_NODELABEL(nrf70_tx_power_ceiling), max_pwr_5g_high_mcs7);
+
+	tx_pwr_ceil_params->max_pwr_5g_low_mcs0 =
+		DT_PROP(DT_NODELABEL(nrf70_tx_power_ceiling), max_pwr_5g_low_mcs0);
+
+	tx_pwr_ceil_params->max_pwr_5g_mid_mcs0 =
+		DT_PROP(DT_NODELABEL(nrf70_tx_power_ceiling), max_pwr_5g_mid_mcs0);
+
+	tx_pwr_ceil_params->max_pwr_5g_high_mcs0 =
+		DT_PROP(DT_NODELABEL(nrf70_tx_power_ceiling), max_pwr_5g_high_mcs0);
+#endif /* CONFIG_NRF70_2_4G_ONLY */
+}
+
+static void configure_board_dep_params(struct nrf_wifi_board_params *board_params)
+{
+	board_params->pcb_loss_2g = CONFIG_NRF700X_PCB_LOSS_2G;
+#ifndef CONFIG_NRF70_2_4G_ONLY
+	board_params->pcb_loss_5g_band1 = CONFIG_NRF700X_PCB_LOSS_5G_BAND1;
+	board_params->pcb_loss_5g_band2 = CONFIG_NRF700X_PCB_LOSS_5G_BAND2;
+	board_params->pcb_loss_5g_band3 = CONFIG_NRF700X_PCB_LOSS_5G_BAND3;
+#endif /* CONFIG_NRF70_2_4G_ONLY */
+}
+
+
 int nrf70_fmac_init(void)
 {
 	enum nrf_wifi_status status = NRF_WIFI_STATUS_FAIL;
@@ -238,7 +338,7 @@ int nrf70_fmac_init(void)
 	void *rpu_ctx = nrf70_bm_priv.rpu_ctx_bm.rpu_ctx;
 	struct nrf_wifi_tx_pwr_ctrl_params tx_pwr_ctrl_params = { 0 };
 	/* TODO: Hardcoded to 10 dBm, take as parameter */
-	struct nrf_wifi_tx_pwr_ceil_params tx_pwr_ceil_params = { 40 };
+	struct nrf_wifi_tx_pwr_ceil_params tx_pwr_ceil_params;
 	struct nrf_wifi_board_params board_params = { 0 };
 	bool enable_bf = false;
 	enum op_band op_band = CONFIG_NRF_WIFI_OP_BAND;
@@ -316,6 +416,11 @@ int nrf70_fmac_init(void)
 		NRF_WIFI_UMAC_VER_MAJ(fw_ver),
 		NRF_WIFI_UMAC_VER_MIN(fw_ver),
 		NRF_WIFI_UMAC_VER_EXTRA(fw_ver));
+
+	configure_tx_pwr_settings(&tx_pwr_ctrl_params,
+				  &tx_pwr_ceil_params);
+
+	configure_board_dep_params(&board_params);
 
 #ifdef CONFIG_NRF_WIFI_BEAMFORMING
 	enable_bf = true;
